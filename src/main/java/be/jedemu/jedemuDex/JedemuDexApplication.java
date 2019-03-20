cb935f2f1b1b540a493253ebd1eed4ed9b80fee8
jeroen <jeroen.demuyt@hotmail.com>
2019-03-20 20:22:22 +0100

    Initializr project

---
+++ src/main/java/be/jedemu/jedemuDex/JedemuDexApplication.java
@@ -0,0 +1,14 @@
+package be.jedemu.jedemuDex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedemuDexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JedemuDexApplication.class, args);
+	}
+
+}
+
