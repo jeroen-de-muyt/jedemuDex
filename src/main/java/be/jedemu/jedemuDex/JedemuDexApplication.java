816fdc53e3725dd9c7ce3cdd1133a529cd79d56f
jeroen <jeroen.demuyt@hotmail.com>
2019-03-20 21:21:31 +0100

    Added hello world page

--- src/main/java/be/jedemu/jedemuDex/JedemuDexApplication.java
+++ src/main/java/be/jedemu/jedemuDex/JedemuDexApplication.java
@@ -3,12 +3,9 @@ package be.jedemu.jedemuDex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "be.jedemu.jedemuDex")
 public class JedemuDexApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JedemuDexApplication.class, args);
 	}
-
 }
-
---
+++ src/main/java/be/jedemu/jedemuDex/infrastructure/web/RootController.java
@@ -0,0 +1,14 @@
+package be.jedemu.jedemuDex.infrastructure.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/")
+public class RootController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
