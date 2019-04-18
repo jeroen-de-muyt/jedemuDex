6a41b8d6ab3d32fa8919f78f26f773b0a2a152c1
jeroen <jeroen@custodix.com>
2019-04-18 16:57:01 +0200

    Let the lorem ipsum component be separated from the general page rendering

--- src/main/react/index.js
+++ src/main/react/index.js
@@ -1,7 +1,8 @@
 import "@babel/polyfill";
 import ReactDOM from "react-dom";
 import PageLayout from "./page-layout.component";
+import LoremIpsum from "./lorem-ipsum.component";
 
 window.appendPageLayout = function (element) {
-    return ReactDOM.render(<PageLayout />, element)
+    return ReactDOM.render(<PageLayout><LoremIpsum/></PageLayout>, element)
 };
--- src/main/react/page-layout.component.js
+++ src/main/react/page-layout.component.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PageHeader from "./page-layout-parts/page-header.component";
 import PageFooter from "./page-layout-parts/page-footer.component";
-import LoremIpsum from "./lorem-ipsum.component";
 
 export default class PageLayout extends React.Component {
     render() {
@@ -11,9 +10,7 @@ export default class PageLayout extends React.Component {
                 <div className="sidebar">
                     SIDEBAR
                 </div>
-                <div className="content">
-                    <LoremIpsum/>
-                </div>
+                <div className="content">{this.props.children}</div>
             </div>
             <PageFooter/>
         </div>)
